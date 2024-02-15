29e2313c238a4d663111a5a6b6216fc58d28f470
mrsamsonn <andrewsj.ahs@gmail.com>
2024-02-14 18:12:06 -0800

    add scroll to section

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,21 @@
 const toggleCheckbox = document.getElementById('Toggle3');
-        const techySection = document.getElementById('techySection');
-        const nonTechySection = document.getElementById('nonTechySection');
+const techySection = document.getElementById('techySection');
+const nonTechySection = document.getElementById('nonTechySection');
 
-        toggleCheckbox.addEventListener('change', function() {
-            if (this.checked) {
-                // Show techy side and hide non-techy side
-                nonTechySection.classList.remove('hidden');
-                techySection.classList.add('hidden');
-            } else {
-                // Show non-techy side and hide techy side
-                nonTechySection.classList.add('hidden');
-                techySection.classList.remove('hidden');
-            }
-        });
+toggleCheckbox.addEventListener('change', function() {
+    if (this.checked) {
+        // Show techy side and hide non-techy side
+        nonTechySection.classList.remove('hidden');
+        techySection.classList.add('hidden');
+    } else {
+        // Show non-techy side and hide techy side
+        nonTechySection.classList.add('hidden');
+        techySection.classList.remove('hidden');
+    }
+});
+
+function scrollToTarget(elementId) {
+    var element = document.getElementById(elementId);
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
+    element.style.marginTop = '100px';
+}
