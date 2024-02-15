0df25825233483bb49e76c29dccefc4acee79a0d
mrsamsonn <andrewsj.ahs@gmail.com>
2024-02-14 22:02:00 -0800

    add side navbar

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function scrollToTarget(elementId) {
     var offset = element.getBoundingClientRect().top + window.pageYOffset - 100;
     window.scrollTo({ top: offset, behavior: "smooth" });
 }
+
+window.addEventListener('scroll', function() {
+    var fixedNavbar = document.getElementById('fixedNavbar');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 300) {
+        fixedNavbar.classList.remove('hidden');
+        fixedNavbar.classList.add('flex');
+    } else {
+        fixedNavbar.classList.remove('flex');
+        fixedNavbar.classList.add('hidden');
+    }
+});
+
+
