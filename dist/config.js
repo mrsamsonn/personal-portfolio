65d618ee41e3f6969364ba9564519967c4232179
mrsamsonn <andrewsj.ahs@gmail.com>
2024-08-24 02:30:03 -0700

    debug config

--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,7 @@
+console.log('SERVICEID:', process.env.SERVICEID);
+console.log('TEMPLATEID:', process.env.TEMPLATEID);
+console.log('PUBLICKEY:', process.env.PUBLICKEY);
+
 module.exports = {
   emailjsServiceId: process.env.SERVICEID,
   emailjsTemplateId: process.env.TEMPLATEID,
