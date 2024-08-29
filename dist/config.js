d4aed6fa6b69d761c59edbc2ff1af8f4f2ffb115
mrsamsonn <andrewsj.ahs@gmail.com>
2024-08-29 10:25:20 -0700

    config bypass working, but incorrect publickey

--- dist/config.js
+++ dist/config.js
@@ -2,6 +2,6 @@ export const emailjsServiceId = '__SERVICEID__';
 export const emailjsTemplateId = '__TEMPLATEID__';
 export const emailjsPublicKey = '__PUBLICKEY__';
 
-console.log(`Service ID: ${emailjsServiceId}`);
-console.log(`Template ID: ${emailjsTemplateId}`); 
-console.log(`Public Key: ${emailjsPublicKey}`); 
+// console.log(`Service ID: ${emailjsServiceId}`);
+// console.log(`Template ID: ${emailjsTemplateId}`); 
+// console.log(`Public Key: ${emailjsPublicKey}`); 
