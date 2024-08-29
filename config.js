1d965f5274f09612d1ddff8f0178bc8d032c2d96
mrsamsonn <andrewsj.ahs@gmail.com>
2024-08-28 22:12:18 -0700

    update configjs

--- config.js
+++ config.js
@@ -1,10 +1,3 @@
 export const emailjsServiceId = process.env.SERVICEID;
 export const emailjsTemplateId = process.env.TEMPLATEID;
 export const emailjsPublicKey = process.env.PUBLICKEY;
-
-import { getMessage } from './contact.js';
-  
-console.log(`Service ID: ${emailjsServiceId}`);
-console.log(`Template ID: ${emailjsTemplateId}`); 
-console.log(`Public Key: ${emailjsPublicKey}`); 
-console.log(`log console: ${getMessage}`); 
